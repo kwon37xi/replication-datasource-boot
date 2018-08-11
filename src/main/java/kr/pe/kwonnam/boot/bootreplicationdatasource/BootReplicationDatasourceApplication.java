a4bdcae1c08affd2fa17480e1614bc8bd10b2959
KwonNam Son <kwon37xi@gmail.com>
2018-08-11 12:54:27 +0900

    initialize project

---
+++ src/main/java/kr/pe/kwonnam/boot/bootreplicationdatasource/BootReplicationDatasourceApplication.java
@@ -0,0 +1,12 @@
+package kr.pe.kwonnam.boot.bootreplicationdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootReplicationDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootReplicationDatasourceApplication.class, args);
+	}
+}
---
+++ src/test/java/kr/pe/kwonnam/boot/bootreplicationdatasource/BootReplicationDatasourceApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.pe.kwonnam.boot.bootreplicationdatasource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootReplicationDatasourceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
